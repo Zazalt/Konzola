bd27b40143f53ccb238175a848ab2f04851c7cda
Zazalt <liviu+zazalt+github.com@sindla.com>
2016-11-28 22:52:21 +0200

    The cake is a lie.

---
+++ tests/KonzolaTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Zazalt\Konzola\Tests;
+
+class KonzolaTest extends \Zazalt\Konzola\Tests\ZazaltTest
+{
+    protected $that;
+
+    public function __construct()
+    {
+        parent::loader($this, []);
+    }
+}
