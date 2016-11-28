036a9b95da07b443e8e5bbe242daace0c2fa8c21
Zazalt <liviu+zazalt+github.com@sindla.com>
2016-11-28 22:37:29 +0200

    The cake is a lie.

---
+++ README.md
@@ -0,0 +1,31 @@
+Konzola
+=================
+
+[![Build Status](https://travis-ci.org/Zazalt/Konzola.svg?branch=master)](https://travis-ci.org/Zazalt/Konzola)
+[![Coverage Status](https://coveralls.io/repos/github/Zazalt/Konzola/badge.svg?branch=master)](https://coveralls.io/github/Zazalt/Konzola?branch=master)
+[![Code Climate](https://codeclimate.com/github/Zazalt/Konzola/badges/gpa.svg)](https://codeclimate.com/github/Zazalt/Konzola)
+[![Issue Count](https://codeclimate.com/github/Zazalt/Konzola/badges/issue_count.svg)](https://codeclimate.com/github/Zazalt/Konzola/issues)
+[![Total Downloads](https://poser.pugx.org/zazalt/konzola/downloads)](https://packagist.org/packages/zazalt/konzola/stats)
+[![Latest Stable Version](https://poser.pugx.org/zazalt/konzola/v/stable)](https://packagist.org/packages/zazalt/konzola)
+![Version](https://img.shields.io/badge/version-beta-yellow.svg)
+
+
+
+Requirements
+---------------
+* php >= 5.4.0
+
+Packagist Dependencies
+---------------
+* None
+
+Installation
+---------------
+With composer:
+``` json
+{
+	"require": {
+		"zazalt/konzola": "dev-master"
+	}
+}
+```
---
+++ src/Konzola.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace Zazalt\Strink;
+
+class Konsola
+{
+    protected $string;
+
+    public function __construct($string)
+    {
+        $this->string = $string;
+    }
+
+    /**
+     * Return the given command
+     */
+    public function getCommand()
+    {
+
+    }
+
+    /**
+     * Return all arguments
+     */
+    public function getArguments()
+    {
+
+    }
+
+    /**
+     * Return a specific argument
+     *
+     * @param $argumentName string
+     */
+    public function getArgument($argumentName)
+    {
+
+    }
+
+    public function text($string)
+    {
+        return new static($string);
+    }
+
+    public function color($color)
+    {
+        switch ($color) {
+            case 'black':
+                $code = 30; break;
+
+            case 'blue':
+                $code = 34; break;
+
+            case 'green':
+                $code = 32; break;
+
+            case 'cyan':
+                $code = 36; break;
+
+            case 'red':
+                $code = 31; break;
+
+            default:
+                $code = 0; break;
+        }
+
+        return new static(sprintf('%s %s %s', "\e[{$code}m", $this->string, "\e[0m"));
+    }
+
+    public function bg($color)
+    {
+        switch ($color) {
+            case 'red':
+                $code = 41; break;
+
+            case 'black':
+            default:
+                $code = 30; break;
+        }
+
+        return new static(sprintf('%s %s %s', "\e[{$code}m", $this->string, "\e[0m"));
+    }
+
+    public function bold()
+    {
+        return new static($this->string);
+    }
+
+    public function tabs($number)
+    {
+        return new static(str_repeat('    ', $number) . $this->string);
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+        return $this->string;
+    }
+}
