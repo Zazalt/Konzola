3013c62db2d5b480d534547e50b531571e51284a
Zazalt <liviu+zazalt+github.com@sindla.com>
2016-11-29 17:06:45 +0200

    The cake is a lie.

--- examples/example1.php
+++ examples/example1.php
@@ -1,4 +1,4 @@
 <?php
-require '../vendor/autoload.php';
+require_once is_dir('../vendor/') ? '../vendor/autoload.php' : 'vendor/autoload.php';
 
 \Zazalt\Konzola\Konzola::text('This is a rainbow.')->rainbow();
